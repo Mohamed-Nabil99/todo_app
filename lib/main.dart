276d4c4bc84dfa2bcc144f00817e79c20e76598a
Windows 10 <moh.nabil4444@gmail.com>
2024-02-13 11:09:03 +0200

    intial project added home screen

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:todo_project/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routName,
+      routes: {
+        HomeScreen.routName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
